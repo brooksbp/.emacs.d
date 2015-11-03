a00f18ef89ee4ae414dd602040e98bb98587476c
Brian Brooks <brooks.brian@gmail.com>
2015-11-03 02:02:39 -0600

    Update cc modes

--- lisp/init-cc.el
+++ lisp/init-cc.el
@@ -1,8 +1,15 @@
 (require-package 'google-c-style)
 
-(add-hook 'c-mode-common-hook 'google-set-c-style)
-(add-hook 'c-mode-common-hook 'google-make-newline-indent)
+(defun setup-c-mode ()
+  (set-variable 'indent-tabs-mode t))
+(add-hook 'c-mode-common-hook 'setup-c-mode)
 
 (setq c-default-style "linux")
+
+(defun google-cc-mode ()
+  (interactive)
+  (set-variable 'indent-tabs-mode nil)
+  (google-set-c-style)
+  (google-make-newline-indent))
 
 (provide 'init-cc)
