894993492c26f06f30bddf1940cba02fab12660a
Brian Brooks <brooks.brian@gmail.com>
2018-05-07 06:37:50 -0500

    rm lisp/init-elpa.el

--- init.el
+++ init.el
@@ -3,6 +3,34 @@
 (add-to-list 'load-path (expand-file-name "lisp" user-emacs-directory))
 (add-to-list 'load-path (expand-file-name "~/Bluespec-2017.03.beta1/util/emacs"))
 
+;;---------------------------------------------------------------------------
+
+(require 'package)
+
+(add-to-list 'package-archives
+             '("melpa" . "https://melpa.org/packages/"))
+
+(when (< emacs-major-version 24)
+  ;; For important compatability libraries like cl-lib
+  (add-to-list 'package-archives '("gnu" . "http://elpa.gnu.org/packages/")))
+
+(setq package-enable-at-startup nil)
+(package-initialize)
+
+(defun require-package (package &optional min-version no-refresh)
+  "Install given 'package', optionally requiring 'min-version'.
+If 'no-refresh' is non-nil, the available package lists will not be
+re-downloaded in order to locate 'package'."
+  (if (package-installed-p package min-version)
+      t
+    (if (or (assoc package package-archive-contents) no-refresh)
+        (package-install package)
+      (progn
+        (package-refresh-contents)
+        (require-package package min-version t)))))
+
+;;---------------------------------------------------------------------------
+
 (setq vc-follow-symlinks t)
 
 (setq inhibit-startup-screen t)
@@ -83,8 +111,6 @@
 
 (global-set-key "\C-xm" 'browse-url-at-point)
 
-
-(require 'init-elpa)
 
 ;(require 'init-org)
 
